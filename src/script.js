5bfac5500ef6391d480ca048449294cf1b0401e2
surenderkumarrajput <surenderkumar2000kumar@gmail.com>
2021-02-22 15:45:50 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -178,15 +178,15 @@ let HeightSlider = document.getElementById("HeightSlider");
 HeightSlider.addEventListener('change', () => {
   if (selectedObject != null) {
     selectedObject.scale.y = [HeightSlider.value];
+    selectedObject.updateMatrix();
   }
-  console.log(HeightSlider.value);
 });
 let widthSlider = document.getElementById("WidthSlider");
 widthSlider.addEventListener('change', () => {
   if (selectedObject != null) {
     selectedObject.scale.x = [widthSlider.value];
+    selectedObject.updateMatrix();
   }
-  console.log(widthSlider.value);
 });
 BindingSelectionEvent();
 init();
